6b67e3f67da3259dd07c9e35c22a025ea3fd9faf
Mika Stengewis <mika@stengewis.nl>
2025-10-31 11:47:55 +0100

    Bg image change

--- opdracht3/nexed/portfol/app/page.tsx
+++ opdracht3/nexed/portfol/app/page.tsx
@@ -2,11 +2,15 @@ import Navbar from "./Navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen font-sans">
+    <div className="h-screen overflow-hidden font-sans">
       <Navbar />
 
-      <main>
-        <img src="/landingpagebg.png" alt="Landing background" className="w-full h-screen object-cover object-bottom block" />
+      <main className="h-full">
+        <img
+          src="/landingpagebg.png"
+          alt="Landing background"
+          className="w-full h-full object-cover object-bottom block"
+        />
       </main>
     </div>
   );
